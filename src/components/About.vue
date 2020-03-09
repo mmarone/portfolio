4dd8e4dede9603cce53d96c181f099d4388481b5
Nataphong <nataphongs_e@hotmail.com>
2020-03-09 18:04:51 +0700

    add about section

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,7 +1,20 @@
 <template>
-  <div id="about" class="about bg-white">
-    This is about Section
-  </div>
+  <section id="about" class="about bg-white">
+    <div class="profile-pic">
+      <img src="@/assets/img/profile.jpg" alt="" />
+    </div>
+    <div class="profile-info">
+      <div class="name">
+        <b>Nataphong Somjai</b>
+      </div>
+      <div class="info">
+        <b>Date of birth :</b> 20 april 1994  <br>
+        <b>Language :</b> Thai ( Native ) , English ( Good ) <br>
+        <b>Tel : </b> 0955592959 <br>
+        <b>Email : </b> nataphong_s@hotmail.com <br>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -9,5 +22,44 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
+.about {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: $color-white;
 
+  background-image: linear-gradient(
+      to right,
+      rgba(56, 70, 77, 0.85),
+      rgba(21, 26, 27, 0.85)
+    ),
+    url(../assets/img/bg2.jpg);
+  background-size: cover;
+  background-attachment: fixed;
+
+  .profile-pic {
+    width: 25em;
+    height: 25rem;
+    overflow: hidden;
+    border-radius: 50%;
+
+    img {
+      width: 100%;
+    }
+  }
+  .profile-info {
+    margin-top: 2rem;
+    .name {
+      font-size: 4.5rem;
+      text-align: center;
+      // border-bottom: 5px solid $color-white;
+      // border-width: 80%;
+    }
+
+    .info {
+      margin-top: 2rem;
+      font-size: 3rem;
+   }
+  }
+}
 </style>
